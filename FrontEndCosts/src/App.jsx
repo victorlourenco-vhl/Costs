f18dedef9daff321c1743cca75c4bcacc7262b49
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-01-16 17:46:36 -0300

    FEAT: Estruturando o projeto: Utilizando CSS opcional e fazendo a configuracao do container

--- FrontEndCosts/src/App.jsx
+++ FrontEndCosts/src/App.jsx
@@ -1,10 +1,35 @@
-function App() {
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
 
+function App() {
   return (
     <div className="App">
       <p>Costs</p>
+      <BrowserRouter>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/company">Empresa</Link>
+          <Link to="/newproject">Novo projeto</Link>
+        </div>
+        <Container customClass="min-height">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/company' element={<Company />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/newproject' element={<NewProject />} />
+          </Routes> 
+        </Container>
+        <p>Footer</p>
+      </BrowserRouter>
+      
     </div>
   )
 }
 
-export default App
+export default App;
